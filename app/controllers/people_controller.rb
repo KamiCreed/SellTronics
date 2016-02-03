6563060463f9b1b7ec51bc17b119b17987c2bd5a
Sami Ma <sami.ma3@gmail.com>
2016-02-03 09:13:20 -0800

    Now added Edit page and links to it

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -11,6 +11,10 @@ class PeopleController < ApplicationController
     @person = Person.new
   end
 
+  def edit
+    @person = Person.find(params[:id])
+  end
+
   def create
     @person = Person.new(person_params)
 
@@ -19,6 +23,16 @@ class PeopleController < ApplicationController
       redirect_to @person
     else
       render 'new'
+    end
+  end
+
+  def update
+    @person = Person.find(params[:id])
+
+    if @person.update(person_params)
+      redirect_to @person
+    else
+      render 'edit'
     end
   end
 

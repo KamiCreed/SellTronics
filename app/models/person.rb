2f6e73800768d739e8d3d3685b29dee89c2df7fe
Sami Ma <sami.ma3@gmail.com>
2016-02-02 22:36:17 -0800

    Added validations to fields in models/person.rb

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,2 +1,6 @@
 class Person < ActiveRecord::Base
+  validates :name, presence = true
+  validates :weight, presence = true, numericality: true
+  validates :height, presence = true, numericality: true
+  validates :colour, presence = true
 end

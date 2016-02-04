700af8be8c98b418cb502863f346d60aa4b39d92
Sami Ma <masamim@sfu.ca>
2016-02-03 21:12:35 -0800

    Add about page and add test for titles in index and about

--- test/controllers/welcome_controller_test.rb
+++ test/controllers/welcome_controller_test.rb
@@ -4,10 +4,12 @@ class WelcomeControllerTest < ActionController::TestCase
   test "should get index" do
     get :index
     assert_response :success
+    assert_select "title", "Welcome | SellTronics"
   end
 
   test "should get about" do
     get :about
     assert_response :success
+    assert_select "title", "About | SellTronics"
   end
 end

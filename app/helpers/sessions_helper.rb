21adb8c60f3bf7d70b1817e4186f1ce21925b575
Sami Ma <masamim@sfu.ca>
2016-02-05 00:24:16 -0800

    Make new layout changes upon login

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,4 +4,14 @@ module SessionsHelper
   def log_in(person)
     session[:person_id] = person.id
   end
+
+  # Returns the current logged-in person (if any).
+  def current_person
+    @current_person ||= Person.find_by(id: session[:person_id])
+  end
+
+  # Returns true if user is logged in, false otherwise.
+  def logged_in?
+    !current_person.nil?
+  end
 end

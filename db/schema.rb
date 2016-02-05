3e0d05bce04cced8b6ce9e601bab07cc0ca4f208
Sami Ma <masamim@sfu.ca>
2016-02-04 18:05:47 -0800

    Fix error when migrating in previous commit

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160205001205) do
+ActiveRecord::Schema.define(version: 20160205020021) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,5 +25,7 @@ ActiveRecord::Schema.define(version: 20160205001205) do
     t.datetime "updated_at"
     t.string   "email"
   end
+
+  add_index "people", ["email"], name: "index_people_on_email", unique: true, using: :btree
 
 end

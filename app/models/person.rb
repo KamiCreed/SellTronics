894c30995681b090ecf3f15e6f710bafd7d91db6
Sami Ma <masamim@sfu.ca>
2016-02-04 18:17:13 -0800

    Add new column password_digest for hashed passwords

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,4 +1,5 @@
 class Person < ActiveRecord::Base
+  before_save {self.email = email.downcase}
   validates :name, presence: true, length: {maximum: 50}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: {maximum: 255},
@@ -6,4 +7,5 @@ class Person < ActiveRecord::Base
                     uniqueness: {case_sensitive: false}
   validates :weight, numericality: true, allow_blank: true
   validates :height, numericality: true, allow_blank: true
+  has_secure_password
 end

962b5461cb264d7ea1bc2a91c40ad829cfa28de1
Sami Ma <sami.ma3@gmail.com>
2016-02-05 16:45:08 -0800

    Add new and edit page for electronics

--- app/controllers/electronics_controller.rb
+++ app/controllers/electronics_controller.rb
@@ -1,10 +1,30 @@
 class ElectronicsController < ApplicationController
   def new
+    @electronic = Electronic.new
+  end
+
+  def edit
+  end
+
+  def create
+    @electronic = Electronic.new(electronic_params)
+
+    # Checks if it is saved in case of bad input
+    if @electronic.save
+      log_in @electronic
+      flash[:success] = "Electronic successfully added!"
+      redirect_to @electronic
+    else # render 'new' and show error messages
+      render 'new'
+    end
   end
 
   def show
   end
 
-  def edit
-  end
+  private
+
+    def electronic_params
+      params.require(:electronic).permit(:name, :desc)
+    end
 end
